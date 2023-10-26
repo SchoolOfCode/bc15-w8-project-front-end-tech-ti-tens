3679905450320869cebd313d1e1c6201a554bca5
Haya Minhas <faithwaheed@icloud.com>
2023-10-26 15:48:25 +0100

    playwright testing user story

---
+++ public/tests/revision.test.js
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+//test:
+//test skeleton
+//navigate to page
+//checked that page has title.
+//click next.
+//user clicks true or false.
+//user clicks next.
+// playwright to recognise different questions to have different answers.
+
+test("practice test", async function ({page}){
+
+await page.goto('http://localhost:5500/bc15-w8-project-front-end-tech-ti-tens/public/')
+
+await expect (page).toHaveTitle(/Tech Ti-tens Revision tool/);
+
+const nextB = page.getByRole('button', { name: 'Next' })
+await expect (nextB).toContainText("Next");
+await nextB.click();
+
+});
